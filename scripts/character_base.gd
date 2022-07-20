9e7ca2cfa64e41383e377c2eb84be44da757ff59
TheoYOSHIURA <theo.yr@gmail.com>
2022-07-20 14:11:16 +0200

    Premières sprites + test scriptable objects

---
+++ scripts/character_base.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name character_stats
+
+export(int) var hp = 100
+export(int) var mp = 100
+export(int) var atk = 100
+export(int) var def = 100
+export(int) var speed = 100
+export(Image) var img = null
